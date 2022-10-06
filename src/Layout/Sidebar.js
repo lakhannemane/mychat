18a00af29168a2e53bd4f8632526a2b691a40baf
laxmanprometteur <lnemane7@gmail.com>
2022-10-06 18:37:08 +0530

    this is chat application icon part is remaing

--- src/Layout/Sidebar.js
+++ src/Layout/Sidebar.js
@@ -2,34 +2,34 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar-logo-section bg-primary ">
+    <div className="sidebar-logo-section chativa-bg-primary">
       <div className="logo-section">
-        <p className="logo text-uppercase text-white fw-7 ">Chat</p>
+        <p className="logo text-uppercase text-white chativa-fw-7 ">Chat</p>
       </div>
 
       <div className="menu-icon text-white d-flex flex-column justify-content-between ">
         <ul className="ul nav-list list-unstyled">
           <li className="nav-item">
-            <a href="/" className="nav-link">
-              <div className="item d-flex flex-column align-items-center ">
-                <i class="fa-solid fa-gauge     sidebar-icon"></i>
-                <p className="  paragraph1">Dashbord</p>
+            <a href="/" className="nav-link active ">
+              <div className="item d-flex flex-column align-items-center  ">
+                <i class="fa-regular fa-gauge chativa-fs-titles mb-2"></i>
+                <p className="chativa-fs-a">Dashbord</p>
               </div>
             </a>
           </li>
           <li className="nav-item">
             <a href="/" className="nav-link">
               <div className="d-flex flex-column align-items-center">
-                <i class="fa-regular fa-comment-dots    sidebar-icon"></i>
-                <p className="  paragraph1">Chats</p>
+                <i class="fa-regular fa-comment-dots chativa-fs-titles mb-2"></i>
+                <p className="chativa-fs-a">Chats</p>
               </div>
             </a>
           </li>
           <li className="nav-item">
             <a href="/" className="nav-link">
               <div className="d-flex flex-column align-items-center">
-                <i class="fa-solid fa-clipboard-list     sidebar-icon"></i>
-                <p className="  paragraph1">Activity</p>
+                <i class="fa-regular fa-clipboard-list   chativa-fs-titles mb-2"></i>
+                <p className="  chativa-fs-a">Activity</p>
               </div>
             </a>
           </li>
@@ -39,24 +39,24 @@ const Sidebar = () => {
           <li className="nav-item">
             <a href="/" className="nav-link">
               <div className="d-flex flex-column align-items-center">
-                <i class="fa-regular fa-circle-user    sidebar-icon"></i>
-                <p className="  paragraph1">Profile</p>
+                <i class="fa-regular fa-circle-user  chativa-fs-titles mb-2"></i>
+                <p className="  chativa-fs-a">Profile</p>
               </div>
             </a>
           </li>
           <li className="nav-item">
             <a href="/" className="nav-link">
               <div className="d-flex flex-column align-items-center">
-                <i class="fa-regular fa-circle-user    sidebar-icon"></i>
-                <p className="  paragraph1">Profile</p>
+                <i class="fa-regular fa-circle-user  chativa-fs-titles mb-2"></i>
+                <p className="  chativa-fs-a">Profile</p>
               </div>
             </a>
           </li>
           <li className="nav-item">
             <a href="/" className="nav-link">
               <div className="d-flex flex-column align-items-center">
-                <i class="fa-solid fa-arrow-right-from-bracket     sidebar-icon"></i>
-                <p className="  paragraph1">Log Out</p>
+                <i class="fa-regular fa-arrow-right-from-bracket   chativa-fs-titles mb-2"></i>
+                <p className="  chativa-fs-a">Log Out</p>
               </div>
             </a>
           </li>
--- src/component/ChatSection.js
+++ src/component/ChatSection.js
@@ -2,11 +2,160 @@ import React from "react";
 
 const ChatSection = () => {
   return (
-    <div className="recent-user-section">
-      {/* heaindg */}
+    <div className="messages-section chativa-bg-chat ">
+      {/* user-name-and-status */}
 
-      <div className="recent-title ">
-        <h5 className="heading first ">Chats</h5>
+      <div className="mx-2">
+
+        <div className="user-name-status ">
+          <div className="chat-user-recent px-3 py-2   ">
+            <div className="d-flex align-items-center ">
+              <div className="chat-img-user align-self-center position-relative me-3">
+                <img
+                  src="http://chatvia-light.react.themesbrand.com/static/media/avatar-2.feb0f89de58f0ef9b424.jpg"
+                  alt=""
+                  className="rounded-circle user-image"
+                />
+              </div>
+              <div className="user-name-message flex-grow-1 overflow-hidden me-auto">
+                <h5 className=" chativa-fourth  chativa-fs-a">Alren richard </h5>
+                <p className="chativa-extra chativa-fs-c">Online</p>
+              </div>
+
+              <div className="user-heaings-icons">
+                <div className="icons-user-header d-flex w-50 justify-content-center">
+                  <div className="serch user-header-icons ">
+
+                  </div>
+                  <div className="call user-header-icons">
+                    <i class="fas fa-phone-alt chativa-extra "></i>
+                  </div>
+                  <div className="video user-header-icons">
+                    <i class="fas fa-video chativa-extra"></i>
+                  </div>
+                  <div className="dots user-header-icons">
+                    <i class="fa-regular fa-ellipsis chativa-extra"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* message-section-field */}
+        <div className="message-field mt-5 chatscrollbar " id="chatscrollbar">
+          <div className="row">
+            <div className="d-flex ">
+              <div className="user-message msg">
+                <p className="client chativa-secondary chativa-fs-b">
+                  Hello, how are you Jacob?{" "}
+                </p>
+              </div>
+            </div>
+            {/* day status */}
+            <div className="  d-flex justify-content-center">
+              <div className="day-informartion">
+                <p className="day  chativa-br">yesterday</p>
+              </div>
+            </div>
+            <div className=" w-75 mx-end d-flex justify-content-right">
+              <div className="user-message msg">
+                <p className="client chativa-secondary chativa-fs-b">
+                  Can you find a house for me at an affordable price?{" "}
+                </p>
+              </div>
+            </div>
+            <div className=" w-75 mx-end d-flex justify-content-right">
+              <div className="user-message msg">
+                <p className="client chativa-secondary chativa-fs-b">
+                  I’II give you a nice commission if you can find it.{" "}
+                </p>
+              </div>
+            </div>
+            <div className=" w-75 ms-auto d-flex justify-content-end ">
+              <div className="your-message">
+                <p className="you chativa-secondary chativa-fs-b">
+                  Hello, Mark I’m fine thank you how are you{" "}
+                </p>
+              </div>
+            </div>
+            <div className=" w-75 mx-end d-flex justify-content-right ">
+              <div className="user-message msg">
+                <p className="client chativa-secondary chativa-fs-b">
+                  hi jacob did you find the affordable house i told you{" "}
+                </p>
+              </div>
+            </div>
+            <div className=" w-75 mx-end d-flex justify-content-right">
+              <div className="user-message msg">
+                <p className="client chativa-secondary chativa-fs-b">
+                  Hello, how are you Jacob?{" "}
+                </p>
+              </div>
+            </div>
+            <div className="w-75 mx-end d-flex justify-content-right">
+              <div className="user-message msg">
+                <p className="client chativa-secondary chativa-fs-b">
+                  Can you find a house for me at an affordable price?{" "}
+                </p>
+              </div>
+            </div>
+            <div className="w-75 mx-end d-flex justify-content-right">
+              <div className="user-message msg">
+                <p className="client chativa-secondary chativa-fs-b">
+                  I’II give you a nice commission if you can find it.{" "}
+                </p>
+              </div>
+            </div>
+            <div className="w-75 ms-auto d-flex justify-content-end">
+              <div className="your-message">
+                <p className="you chativa-secondary chativa-fs-b">
+                  Hello, Mark I’m fine thank you how are you{" "}
+                </p>
+              </div>
+            </div>
+            {/* day status */}
+            <div className="d-flex justify-content-center ">
+              <div className="day-informartion">
+                <p className="day chativa-br">Today</p>
+              </div>
+            </div>
+
+            <div className="w-75 mx-end ">
+              <div className="user-message msg">
+                <p className="client chativa-secondary chativa-fs-b">
+                  hi jacob did you find the affordable house i told you hi jacob
+                  did you find the affordable house i told you{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* message send input */}
+
+        <div className="message-sent-section w-100 ">
+          <div className="d-flex">
+            <div className="form-for-send-message ">
+              <form>
+                <input
+                  type="text"
+                  placeholder="Enter message here"
+                  className="
+            sent-message-input chativa-br  "
+                />
+              </form>
+            </div>
+            {/* <div className="icons-send-section1 d-flex ">
+            <div className="attach-icon icons-send-section ">
+              <i class="fa-regular fa-paperclip"></i>
+            </div>
+            <div className="smily-icon icons-send-section">
+              <i class="far fa-smile"></i>
+            </div>
+          </div> */}
+          </div>
+        </div>
       </div>
     </div>
   );
